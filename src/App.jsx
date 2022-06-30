d43a725bbff04321d38c4a55659024dd926055f7
unknown <hemrcs@gmail.com>
2022-06-30 01:53:46 -0300

    000/Project Setup
    - npm create vite
    - Create folder structures
    - Add styles to the index.css

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import LetterApp from "./components/LetterApp";
+
+function App() {
+  return <LetterApp />;
+}
+
+export default App;
---
+++ src/components/LetterApp.jsx
@@ -0,0 +1,13 @@
+import Form from "./Form";
+
+const LetterApp = () => {
+  return (
+    <>
+      <header>Búsqueda de Letras de Canciones</header>
+      <Form />
+      <main></main>
+    </>
+  );
+};
+
+export default LetterApp;
